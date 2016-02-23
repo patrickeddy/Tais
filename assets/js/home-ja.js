c0a24552a637dce604ebd504d805ce8b30724158
patrickeddy <p@patrickeddy.com>
2016-02-23 14:50:10 -0800

    Test.

--- assets/js/home-ja.js
+++ assets/js/home-ja.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   // Change the text to Japanese.
+  alert("Japanese phone.");
   window.title = "タイス - 他の言語を使って、自分自身を表現しよう";
   $("#tais-title-header").text("タイス");
   $("#tais-title-body").text("他の言語を使って、自分自身を表現しよう");
