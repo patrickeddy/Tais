c80a0e35ec5ac2fc626747825fbd00b34f5f9ac1
patrickeddy <p@patrickeddy.com>
2016-02-23 14:41:27 -0800

    Japanese localising...

---
+++ assets/js/home.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  // Change the text to English.
+  window.title("Tais - Express Yourself in Another Language");
+  $("#tais-title-header").text("Tais");
+  $("#tais-title-body").text("Express yourself in another language.");
+  $("#tais-start-button").text("Start");
+});
