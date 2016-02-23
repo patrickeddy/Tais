39f0f2868c4c634a56049e8f18b6e564d81ff2e5
patrickeddy <p@patrickeddy.com>
2016-02-23 14:46:45 -0800

    window.title() is not a function -_-.

--- assets/js/home-ja.js
+++ assets/js/home-ja.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // Change the text to Japanese.
-  window.title("タイス - 他の言語を使って、自分自身を表現しよう");
+  window.title = "タイス - 他の言語を使って、自分自身を表現しよう";
   $("#tais-title-header").text("タイス");
   $("#tais-title-body").text("他の言語を使って、自分自身を表現しよう");
   $("#tais-start-button").text("スタート");
--- assets/js/home.js
+++ assets/js/home.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // Change the text to English.
-  window.title("Tais - Express Yourself in Another Language");
+  window.title = "Tais - Express Yourself in Another Language";
   $("#tais-title-header").text("Tais");
   $("#tais-title-body").text("Express yourself in another language.");
   $("#tais-start-button").text("Start");
